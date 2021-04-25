01f25d05007cc0ce8bd1546be6e9ac14cb9f1b99
lewi0332 <derrickjameslewis@gmail.com>
2021-04-25 16:03:41 -0400

    initial commit

--- data/process_data.py
+++ data/process_data.py
@@ -1,17 +1,75 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """
+    Reads two CSV files for disaster relief classification
+
+    Input - 
+        messages_filepath: filepath to csv with raw text from messages and tweets
+        categories_filepath: filepath to csv file with category labels for 
+                             previous message file
+    Output - Single pandas DataFrame with combined data from files
+    """
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories, on='id')
+    return df
 
 
 def clean_data(df):
-    pass
+    """
+    Prepares data for machine learning task
+
+    Input: Pandas DataFrame with disaster response messages and 
+           labeled classifications
+
+    Output: Pandas DataFrame with expanded one-hot encoded label 
+            classifications as integers
+    """
+
+    # Separate Category column
+    categories = df['categories'].str.split(';', expand = True)
+    # select the first row of the categories dataframe to get label names
+    row = categories.values[0]
+    category_colnames = list(map(lambda x: x[:-2], row))
+    categories.columns = category_colnames
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str[-1:]
+        
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+
+    # Drop any columns that zero positive results.
+    zero_cols = [ col for col, is_zero in ((categories == 0).sum() == categories.shape[0]).items() if is_zero ]
+    categories.drop(zero_cols, axis=1, inplace=True)
+
+    # Drop original catgories column and replace with new expanded and cleaned columns
+    df.drop('categories', axis = 1, inplace=True)
+    df = pd.concat([df, categories], axis=1)
+
+    # Drop Duplicates
+    df.drop_duplicates(inplace=True)
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    """
+    Saves cleaned data to sqlite database. 
 
+    Input - Cleaned pandas DataFrame ready for ML task
+    Output - Message of outcome of data loaded to database
+    """
+    table = database_filename.split('/')[1].split('.')[0]
+    try:
+        engine = create_engine(f'sqlite:///{database_filename}')
+        df.to_sql(table, engine, index=False, if_exists='replace')
+        return f"Successfully added {len(df)} rows to {database_filename}"
+    except Exception as e: 
+        return print(e)
 
 def main():
     if len(sys.argv) == 4:
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,23 +1,101 @@
 import sys
+import pickle as pkl
+# import libraries
+import nltk
+nltk.download(['punkt', 'wordnet'])
+
+import re
+import time
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer 
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.svm import LinearSVC
 
 
 def load_data(database_filepath):
-    pass
+    """
+    Loads data from database
 
+    Input - database_filepath: filepath to sqlite database
+    Output - X, y: Pandas DataFrames with Data and labels for training.
+    """
+    # get table name from filepath
+    table = database_filepath.split('/')[1].split('.')[0]
+
+    engine = create_engine(f'sqlite:///{database_filepath}')
+    df = pd.read_sql_table(table, engine)
+    X = df['message']
+    y = df.drop(['id', 'message', 'original', 'genre'], axis=1)
+    return X, y
 
 def tokenize(text):
-    pass
+    """
+    Tokenize with NLTK and removes URLs
+
+    Input - text - Single string object with english message
+    Output - list of lowercase, lemmatized word tokens
+    """
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+
+    # get list of all urls using regex
+    detected_urls = re.findall(url_regex, text)
+    
+    # replace each url in text string with placeholder
+    for url in detected_urls:
+        text = text.replace(url, 'urlplaceholder')
+
+    # tokenize text
+    tokens = word_tokenize(text)
+    
+    # initiate lemmatizer
+    lemmatizer = WordNetLemmatizer()
+
+    # iterate through each token
+    clean_tokens = []
+    for tok in tokens:
+        
+        # lemmatize, normalize case, and remove leading/trailing white space
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+        
+    return clean_tokens
 
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(LinearSVC()))
+        ])
+    parameters = {
+        'vect__max_df': (0.5, 0.75, 1.0),
+        'tfidf__use_idf': (True, False),
+        'clf__estimator__C': [.5, .75, 1.0],
+        # 'clf__estimator__min_samples_split': [2, 3]
+    }
+
+    model = GridSearchCV(pipeline, param_grid=parameters, verbose=3, cv=3)
+    return model
 
 
-def evaluate_model(model, X_test, Y_test, category_names):
-    pass
-
+def evaluate_model(model, X_test, y_test):
+    y_pred = model.predict(X_test)
+    results_df=pd.DataFrame(columns=['precision', 'recall', 'f1-score', 'support', 'accuracy'])
+    for index, column in enumerate(y_test.columns):
+        cr_dict = classification_report(y_test[column], y_pred[:,index], output_dict=True, labels=np.unique(y_pred[:,index]))
+        cr_dict['weighted avg']['accuracy'] = accuracy_score(y_test[column], y_pred[:,index])
+        results_df = results_df.append(pd.DataFrame(index=[column], data=cr_dict['weighted avg']))
+    return results_df
 
 def save_model(model, model_filepath):
+    pkl.dump(model, open(model_filepath,'wb'))
     pass
 
 
@@ -25,17 +103,18 @@ def main():
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
-        X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        X, y = load_data(database_filepath)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
         
         print('Building model...')
         model = build_model()
         
         print('Training model...')
-        model.fit(X_train, Y_train)
+        model.fit(X_train, y_train)
         
         print('Evaluating model...')
-        evaluate_model(model, X_test, Y_test, category_names)
+        results_df = evaluate_model(model, X_test, y_test)
+        print("Model results: ", results_df.mean())
 
         print('Saving model...\n    MODEL: {}'.format(model_filepath))
         save_model(model, model_filepath)
